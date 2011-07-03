cb5a96cace8191edad20ed498f73936021d7bcb0
Moritz Schmale <narrow.m@gmail.com>
2011-07-03 21:43:25 +0200

    Added TutorialShowcaseExtra to git.

---
+++ src/de/moritzschmale/Showcase/Types/TutorialShowcaseExtra.java
@@ -0,0 +1,40 @@
+package de.moritzschmale.Showcase.Types;
+
+import de.moritzschmale.Showcase.ShowcaseExtra;
+import de.moritzschmale.Showcase.ShowcaseItem;
+import de.moritzschmale.Showcase.ShowcasePlayer;
+
+public class TutorialShowcaseExtra implements ShowcaseExtra {
+
+	private String text = "I've got no text";
+	@SuppressWarnings("unused")
+	private ShowcaseItem showcase = null;
+	
+	@Override
+	public boolean onDestroy(ShowcasePlayer player) {
+		return true;
+	}
+
+	@Override
+	public void onClick(ShowcasePlayer player) {
+		player.sendMessage(text);
+	}
+
+	@Override
+	public String save() {
+		return text.replace("\n", "\\n");
+	}
+
+	@Override
+	public void setShowcaseItem(ShowcaseItem item) {
+		showcase = item;
+	}
+	
+	public void setText(String text){
+		this.text = text.replace("\\n", "\n");
+	}
+	
+	public String getText(){
+		return text;
+	}
+}
